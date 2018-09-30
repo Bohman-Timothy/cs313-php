c07499be62e57d59c44e4e777ab145132e24d430
asquire <32476451+asquire@users.noreply.github.com>
2018-09-29 23:19:12 -0400

    Fixed figcaptions resizing.

--- web/main.js
+++ web/main.js
@@ -1,13 +1,13 @@
 function biggerSupermanImg(supermanImg) {
     supermanImg.width = "400";
     supermanImg.height = "505";
-    document.getElementById("supermanFigCaption").style.width = "400";
+    document.getElementById("supermanFigCaption").style.width = "400px";
     return 0;
 }
 
 function smallerSupermanImg(supermanImg) {
     supermanImg.width = "260";
     supermanImg.height = "328";
-    document.getElementById("supermanFigCaption").style.width = "260";
+    document.getElementById("supermanFigCaption").style.width = "260px";
     return 0;
 }
