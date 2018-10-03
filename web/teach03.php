157a2484a3ebf88ec79032df7b4a1b8f0a67f8fd
asquire <32476451+asquire@users.noreply.github.com>
2018-10-03 19:44:00 -0400

    Added missing form parts and details

--- web/teach03.php
+++ web/teach03.php
@@ -6,12 +6,15 @@
 <!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 </form> -->
 <form method="post" action="teach03_submit.php">
-<input type="text" name="name"><br>
-<input type="text" name="email"><br>
-<input type="radio" name="major" value="Computer Science">Computer Science
-<input type="radio" name="major" value="Web Design and Development">Web Design and Development
-<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology
-<input type="radio" name="major" value="Computer Engineering">Computer Engineering
+<b>Name:</b> <input type="text" name="name"><br>
+<b>Email:</b> <input type="text" name="email"><br>
+<b>Major:</b><br>
+<input type="radio" name="major" value="Computer Science">Computer Science<br>
+<input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
+<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
+<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+<b>Comments:</b><br>
+<textarea name="comments" rows="5" cols="40"></textarea>
 <input type="submit">
 </form>
 </body>
