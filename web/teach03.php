0905615b3599cf348cd43646595aa39d2fa0627a
asquire <32476451+asquire@users.noreply.github.com>
2018-10-03 19:35:59 -0400

    Built the appearance of a form for Teach03

---
+++ web/teach03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+</form> -->
+<form method="post" action="teach03_submit.php">
+<input type="text" name="name"><br>
+<input type="text" name="email"><br>
+<input type="radio" name="major" value="Computer Science">Computer Science
+<input type="radio" name="major" value="Web Design and Development">Web Design and Development
+<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology
+<input type="radio" name="major" value="Computer Engineering">Computer Engineering
+<input type="submit">
+</form>
+</body>
+</html>
