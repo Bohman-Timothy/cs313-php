32c41dffe8be696e9a0cb5d66670d1ea9ac89cd7
asquire <32476451+asquire@users.noreply.github.com>
2018-10-04 00:24:16 -0400

    Added Antarctica to continentsMap

--- web/teach03.php
+++ web/teach03.php
@@ -7,7 +7,7 @@
 <?php
 $name = $email = $major = $comments = $continents = "";
 $majors = array("Computer Science", "Web Design and Development", "Computer information Technology", "Computer Engineering");
-$continentsMap = array("na"=>"North America","sa"=>"South America","eu"=>"Europe","as"=>"Asia","au"=>"Australia","af"=>"Africa");
+$continentsMap = array("na"=>"North America","sa"=>"South America","eu"=>"Europe","as"=>"Asia","au"=>"Australia","af"=>"Africa","an"=>"Antarctica");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $name = clean_input($_POST["name"]);
@@ -36,6 +36,12 @@ function clean_input($data) {
 <b>Name:</b> <input type="text" name="name" value="<?php echo $name;?>"><br>
 <b>Email:</b> <input type="text" name="email" value="<?php echo $email;?>"><br><br>
 <b>Major:</b><br>
+<!--
+<input type="radio" name="major" value="Computer Science">Computer Science<br>
+<input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
+<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
+<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br><br>
+ -->
 <?php
 foreach ($majors as $majorss=>$value) {
 	echo "<input type=\"radio\" name=\"major\" value=\"" . $value . "\">" . $value . "<br>";
@@ -43,23 +49,9 @@ foreach ($majors as $majorss=>$value) {
 }
 ?>
 <br>
-<!--
-<input type="radio" name="major" value="Computer Science">Computer Science<br>
-<input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
-<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
-<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br><br>
- -->
 <b>Comments:</b><br>
 <textarea name="comments" rows="5" cols="40"><?php echo $comments?></textarea><br><br>
 <b>Continents Visited:</b><br>
-<input type="checkbox" name="continents[]" value="na">North America<br>
-<input type="checkbox" name="continents[]" value="sa">South America<br>
-<input type="checkbox" name="continents[]" value="eu">Europe<br>
-<input type="checkbox" name="continents[]" value="as">Asia<br>
-<input type="checkbox" name="continents[]" value="au">Australia<br>
-<input type="checkbox" name="continents[]" value="af">Africa<br>
-<input type="checkbox" name="continents[]" value="an">Antarctica<br>
-
 <!--
 <input type="checkbox" name="continents[]" value="North America">North America<br>
 <input type="checkbox" name="continents[]" value="South America">South America<br>
@@ -69,6 +61,15 @@ foreach ($majors as $majorss=>$value) {
 <input type="checkbox" name="continents[]" value="Africa">Africa<br>
 <input type="checkbox" name="continents[]" value="Antarctica">Antarctica<br>
 -->
+
+<input type="checkbox" name="continents[]" value="na">North America<br>
+<input type="checkbox" name="continents[]" value="sa">South America<br>
+<input type="checkbox" name="continents[]" value="eu">Europe<br>
+<input type="checkbox" name="continents[]" value="as">Asia<br>
+<input type="checkbox" name="continents[]" value="au">Australia<br>
+<input type="checkbox" name="continents[]" value="af">Africa<br>
+<input type="checkbox" name="continents[]" value="an">Antarctica<br><br>
+
 <input type="submit"><br><br>
 </form>
 
