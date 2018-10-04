d02447f60da5b2c875b27ccde9d02119ad09fcc4
asquire <32476451+asquire@users.noreply.github.com>
2018-10-03 20:21:57 -0400

    Form now displays results

--- web/teach03.php
+++ web/teach03.php
@@ -1,21 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title>Teach 03 Form</title>
 </head>
 <body>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $name = clean_input($_POST["name"]);
+   $email = clean_input($_POST["email"]);
+   $major = clean_input($_POST["major"]);
+   $comments = clean_input($_POST["comments"]);
+}
+
+function clean_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
+
 <!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 </form> -->
-<form method="post" action="teach03_submit.php">
+<form method="post" action="<?php echo htmlspecialchars(teach03_submit.php);?>">
 <b>Name:</b> <input type="text" name="name"><br>
-<b>Email:</b> <input type="text" name="email"><br>
+<b>Email:</b> <input type="text" name="email"><br><br>
 <b>Major:</b><br>
 <input type="radio" name="major" value="Computer Science">Computer Science<br>
 <input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
 <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
-<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br><br>
 <b>Comments:</b><br>
-<textarea name="comments" rows="5" cols="40"></textarea>
-<input type="submit">
+<textarea name="comments" rows="5" cols="40"></textarea><br>
+<input type="submit"><br><br>
 </form>
+
+<?php
+echo "<h2>Input Submitted:</h2>";
+echo $name . "<br>";
+echo $email . "<br>";
+echo $major . "br>";
+echo $comments . "<br>";
+?>
 </body>
 </html>
