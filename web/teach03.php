f3d72ce32d2a3985ccd8a89b3acd9a89622fc0d7
asquire <32476451+asquire@users.noreply.github.com>
2018-10-03 23:06:25 -0400

    Testing 8: variable assignment in foreach loop

--- web/teach03.php
+++ web/teach03.php
@@ -12,12 +12,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $email = clean_input($_POST["email"]);
    $major = clean_input($_POST["major"]);
    $comments = clean_input($_POST["comments"]);
-   $continents = clean_input($_POST["continents"]);
-   /*foreach ($continents as $continentss=>$value) {
-   	   echo "1. " . $value;
+   $continents = $_POST["continents"];
+   foreach ($continents as $continentss=>$value) {
+   	   echo "Test before cleaning:1. " . $value;
    	   $value = clean_input($_POST["name"]);
-   	   echo "<br>2. " . $value;
-	   }*/
+   	   echo "<br>Test after cleaning:2. " . $value;
+	   }
 }
 
 function clean_input($data) {
