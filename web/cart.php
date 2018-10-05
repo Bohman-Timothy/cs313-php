30423d851eceeacb3c2f9fe9e289807368b99409
asquire <32476451+asquire@users.noreply.github.com>
2018-10-05 01:35:22 -0400

    Test form data 8 - Added missing closing bracket

--- web/cart.php
+++ web/cart.php
@@ -41,15 +41,13 @@
 ?>
 -->
 <h1>Shopping Cart</h1>
-<!--
 <?php
 echo "<ul>"
 /*foreach ($musicAlbums as $musicAlbumss=>$value) {
 	echo "<li>" . $musicMap[$value] . "</li>";
 }*/
-//echo "<li>$_POST["test_text"</li>";
+echo "<li>$_POST["test_text"</li>";
 echo "</ul>"
 ?>
--->
 </body>
 </html>
