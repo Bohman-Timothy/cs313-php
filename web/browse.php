a5ed864112f660119e7ccede07bd11a1b2a86f58
asquire <32476451+asquire@users.noreply.github.com>
2018-10-05 06:12:01 -0400

    Removed (commented out) unneeded JavaScript functions

--- web/browse.php
+++ web/browse.php
@@ -24,15 +24,6 @@ function addToCartSession($musicAlbums) {
 
 ?>
 <script>
-function addToCartB() {
-	 alert("<?php displayCount('z'); ?>");
-	 /* <?php displayCount('z'); ?>
-	 alert("Added to cart!"); */
-	 var invisibleDiv = document.createElement('div');
-	 invisibleDiv.innerHTML = "<?php displayCount('z'); ?>";
-	 invisibleDiv.className = "hide";
-	 document.body.appendChild(invisibleDiv);
-}
 </script>
 
 <!DOCTYPE html>
--- web/shopping.js
+++ web/shopping.js
@@ -1,6 +1,16 @@
-function addToCart(selectedAlbum) {
+/*function addToCart(selectedAlbum) {
     //$_SESSION["inCart"] = document.getElementById(selectedAlbum).value;
     alert("<?php addToCartSession('z'); ?>");
     alert("Added to cart!");
     return 0;
 }
+
+function addToCartB() {
+	 alert("<?php displayCount('z'); ?>");
+	 /* <?php displayCount('z'); ?>
+	 alert("Added to cart!"); */
+/*	 var invisibleDiv = document.createElement('div');
+	 invisibleDiv.innerHTML = "<?php displayCount('z'); ?>";
+	 invisibleDiv.className = "hide";
+	 document.body.appendChild(invisibleDiv);
+}*/
