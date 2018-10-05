fa50899588fb993b0b0b94cd8671fe413f7d68fe
asquire <32476451+asquire@users.noreply.github.com>
2018-10-05 01:40:03 -0400

    Test form data 11

--- web/cart.php
+++ web/cart.php
@@ -1,3 +1,18 @@
+<?php
+$musicMap = array("beatles_1"=>"The Beatles: <i>1</i>",
+"belindac_runaway"=>"Belinda Carlisle: <i>Runaway Horses</i>",
+"shakira_ladrones"=>"Shakira: <i>D&#243;nde Est&#225;n Los Ladrones?",
+"michelleb_hotel"=>"Michelle Branch: <i>Hotel Paper</i>",
+"lindsey_stirling"=>"Lindsey Stirling: <i>Lindsey Stirling</i>",
+"kellyc_allwanted"=>"Kelly Clarkson: <i>All I Ever Wanted",
+"enya_shepherd"=>"Enya: <i>Shepherd Moons</i>",
+"celined_decade"=>"Celine Dion: <i>All the Way... A Decade of Song</i>",
+"a-ha_hunting"=>"A-ha: <i>Hunting High and Low</i>",
+"sarabar_kaleidoscope"=>"Sara Bareilles: <i>Kaleidoscope Heart</i>",
+"mamaspapas_leaves"=>"The Mamas and the Papas: <i>All The Leaves Are Brown: The Golden Era Collection</i>",
+"tmbgiants_flood"=>"They Might Be Giants: <i>Flood</i>",
+"shania_comeover"=>"Shania Twain: <i>Come on Over</i>");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,19 +36,6 @@
 "mamaspapas_leaves"=>"The Mamas and the Papas: All The Leaves Are Brown: The Golden Era Collection",
 "tmbgiants_flood"=>"They Might Be Giants: Flood",
 "shania_comeover"=>"Shania Twain: Come on Over");*/
-/*$musicMap = array("beatles_1"=>"The Beatles: <i>1</i>",
-"belindac_runaway"=>"Belinda Carlisle: <i>Runaway Horses</i>",
-"shakira_ladrones"=>"Shakira: <i>D&#243;nde Est&#225;n Los Ladrones?",
-"michelleb_hotel"=>"Michelle Branch: <i>Hotel Paper</i>",
-"lindsey_stirling"=>"Lindsey Stirling: <i>Lindsey Stirling</i>",
-"kellyc_allwanted"=>"Kelly Clarkson: <i>All I Ever Wanted",
-"enya_shepherd"=>"Enya: <i>Shepherd Moons</i>",
-"celined_decade"=>"Celine Dion: <i>All the Way... A Decade of Song</i>",
-"a-ha_hunting"=>"A-ha: <i>Hunting High and Low</i>",
-"sarabar_kaleidoscope"=>"Sara Bareilles: <i>Kaleidoscope Heart</i>",
-"mamaspapas_leaves"=>"The Mamas and the Papas: <i>All The Leaves Are Brown: The Golden Era Collection</i>",
-"tmbgiants_flood"=>"They Might Be Giants: <i>Flood</i>",
-"shania_comeover"=>"Shania Twain: <i>Come on Over</i>");*/
 
 //if ($_SERVER["REQUEST_METHOD"] == "POST") {
    //$musicAlbums = $_POST["musicAlbums"];
@@ -41,10 +43,12 @@
 
 -->
 <h1>Shopping Cart</h1>
-<?php
+<!--
+
 echo "<ul>"
 echo "<li>$_POST["test_text"</li>";
 echo "</ul>"
-?>
+
+-->
 </body>
 </html>
