e49a74e0f850f31e2c6577c880c87d401f6d4c36
asquire <32476451+asquire@users.noreply.github.com>
2018-10-05 20:42:10 -0400

    Put full array declaration in browse page

--- web/shopping.php
+++ web/shopping.php
@@ -1,19 +1,5 @@
 <?php
 
-$musicMap = array("a-ha_hunting"=>"A-ha: <i>Hunting High and Low</i>",
-"sarabar_kaleidoscope"=>"Sara Bareilles: <i>Kaleidoscope Heart</i>",
-"beatles_1"=>"The Beatles: <i>1</i>",
-"michelleb_hotel"=>"Michelle Branch: <i>Hotel Paper</i>",
-"belindac_runaway"=>"Belinda Carlisle: <i>Runaway Horses</i>",
-"kellyc_allwanted"=>"Kelly Clarkson: <i>All I Ever Wanted",
-"celined_decade"=>"Celine Dion: <i>All the Way... A Decade of Song</i>",
-"enya_shepherd"=>"Enya: <i>Shepherd Moons</i>",
-"mamaspapas_leaves"=>"The Mamas and the Papas: <i>All The Leaves Are Brown: The Golden Era Collection</i>",
-"shakira_ladrones"=>"Shakira: <i>D&#243;nde Est&#225;n Los Ladrones?</i>",
-"shania_comeover"=>"Shania Twain: <i>Come on Over</i>",
-"lindsey_stirling"=>"Lindsey Stirling: <i>Lindsey Stirling</i>",
-"tmbgiants_flood"=>"They Might Be Giants: <i>Flood</i>");
-
 function clean_input($data) {
    $data = trim($data);
    $data = stripslashes($data);
