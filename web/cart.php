b84fc4fd6dd6c14c3708913bf37e5b016e4bcb46
asquire <32476451+asquire@users.noreply.github.com>
2018-10-05 22:18:43 -0400

    Removed all but session_start

--- web/cart.php
+++ web/cart.php
@@ -1,33 +1,3 @@
 <?php
 session_start();
-
-//$musicAlbums = "";
-
-//include 'shopping.php';
-
-/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $musicAlbums = $_POST["musicAlbums"];
-   $test_text = $_POST["test_text"];
-}*/
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Shopping Cart</title>
-</head>
-<body>
-<h1>Shopping Cart</h1>
-<h2>Items in your cart:</h2>
-<?php
-/*echo "<ul>";
-foreach ($musicAlbums as $musicAlbums=>$value) {
-	echo "<li>" . $musicMap[$value] . "</li>";
-}
-echo "</ul>";*/
-<h2>Items in cart (session):</h2>
-//print_r($_SESSION);
-//echo $_SESSION["inCart"];
-?>
-</body>
-</html>
