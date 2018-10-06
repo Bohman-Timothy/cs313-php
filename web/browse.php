4b36a918cee9e7898e330bc281b3e98d3ac448a6
asquire <32476451+asquire@users.noreply.github.com>
2018-10-06 01:31:39 -0400

    Added foreach loop to set quantities to 0 (temporarily 8)

--- web/browse.php
+++ web/browse.php
@@ -45,6 +45,12 @@ if (!isset($_SESSION["tmbgiants_flood"])) {
       $_SESSION["tmbgiants_flood"] = 0;
 }
 
+foreach ($musicMap as $albumKey=>$fullName) {
+	if (!isset($_SESSION[$albumKey])) {
+		$_SESSION[$albumKey] = 8;
+	}
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $musicAlbum = clean_input($_POST["musicAlbum"]);
    if (isset($musicAlbum) && ($musicAlbum != "")) {
