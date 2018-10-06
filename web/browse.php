99ffd43277ff7dc44ea21cc02f605c4f070faa4a
asquire <32476451+asquire@users.noreply.github.com>
2018-10-06 17:43:59 -0400

    Added Bootstrap stylesheet

--- web/browse.php
+++ web/browse.php
@@ -84,6 +84,10 @@ function addToCartSession($musicAlbum) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
+    <!-- Bootstrap compiled CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="shopping.css">
     <!-- <script src="shopping.js" charset="UTF-8"></script> -->
 
