543cef4b205b862e77a7bac4f659e0f1af76a083
asquire <32476451+asquire@users.noreply.github.com>
2018-10-06 15:38:31 -0400

    Filled in checkout page and a new confirmation page

--- web/checkout.php
+++ web/checkout.php
@@ -11,5 +11,17 @@ session_start();
 </head>
 <body>
 <h1>Checkout</h1>
+<h2>Please enter your shipping address</h2>
+<form method="post" action="<?php echo htmlspecialchars("confirmation.php");?>">
+<label for="street">Street</label>
+<input type="text" name="street"><br />
+<label for="city">City</label>
+<input type="text" name="city"><br />
+<label for="state">State</label>
+<input type="text" name="state">
+<label for="zipCode">Zip Code</label>
+<input type="text" name="zipCode"><br />
+<input type="submit" value="Confirm purchase">
+</form>
 </body>
 </html>
---
+++ web/confirmation.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+include 'shopping.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$street = clean_input($_SESSION['street']);
+	$city = clean_input($_SESSION['city']);
+	$state = clean_input($_SESSION['state']);
+	$zipCode = clean_input($_SESSION['zipCode']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="shopping.css">
+<title>Purchase Confirmed</title>
+</head>
+<body>
+<?php
+echo "<h1>Purchase Confirmed</h1>";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	echo "<ul class="itemsPurchased">";
+	foreach ($musicMap as $albumKey=>$fullName) {
+		$quantity = clean_input($_SESSION[$albumKey]);
+		if ($quantity > 0) {
+			echo "<li><span class='quantity'>" . $quantity . "</span> of <span class='artistAndAlbum'>" . $fullName . "</span></li>";
+		}
+	}
+	echo "Shipping Address:";
+	echo $street;
+	echo $city;
+	echo $state . " " . $zipCode;
+}
+?>
+</body>
+</html>
