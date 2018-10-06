ef4b362ce9f409d900191af956b58e14bc80b4ac
asquire <32476451+asquire@users.noreply.github.com>
2018-10-06 01:25:26 -0400

    Added placeholder checkout page

---
+++ web/checkout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="shopping.css">
+<title>Checkout</title>
+</head>
+<body>
+<h1>Checkout</h1>
+</body>
+</html>
