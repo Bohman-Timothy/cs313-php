5d1a66db496e37f81bb76379573ccab1780f2257
asquire <32476451+asquire@users.noreply.github.com>
2018-10-06 19:33:47 -0400

    Added top and bottom margins to update quantity form list

--- web/cart.php
+++ web/cart.php
@@ -56,7 +56,7 @@ $totalQuantity = 0;
 foreach ($musicMap as $albumKey=>$fullName) {
 	$quantity = $_SESSION[$albumKey];
 	if ($quantity > 0) {
-		echo '<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">';
+		echo '<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '" class="albumQuantityUpdateForm">';
 		echo '<input type="submit" value="Update quantity">';
 		echo '<input type="number" min="' . $minQuantity . '" max="' . $maxQuantity . '" name="quantity" id="' . $albumKey . '_quantity" value="' . $quantity . '">';
 		echo '<label for="albumQuantity">' . $fullName . '</label>';
