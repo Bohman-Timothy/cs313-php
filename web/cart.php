bcc951f34281fc955a65a7aa0cbab40b8fcaf97e
asquire <32476451+asquire@users.noreply.github.com>
2018-10-05 22:20:15 -0400

    Added back in some commented out PHP code

--- web/cart.php
+++ web/cart.php
@@ -1,3 +1,12 @@
 <?php
 session_start();
+
+//$musicAlbums = "";
+
+//include 'shopping.php';
+
+/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $musicAlbums = $_POST["musicAlbums"];
+   $test_text = $_POST["test_text"];
+}*/
 ?>
