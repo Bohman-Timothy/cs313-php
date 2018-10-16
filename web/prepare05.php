cee8a9e3502bf95c94cd44ef0349f58910dac177
asquire <32476451+asquire@users.noreply.github.com>
2018-10-16 19:39:13 -0400

    Added 2 other versions of same query

--- web/prepare05.php
+++ web/prepare05.php
@@ -21,10 +21,22 @@ catch (PDOException $ex)
 	die();
 }
 
+echo 'query version 1:<br />';
 foreach ($db->query('SELECT username, password FROM note_user') as $row)
 {
 	echo 'user: ' . $row['username'];
 	echo ' password: ' . $row['password'];
 	echo '<br/>';
 }
+
+echo '<br />query version 2:<br />';
+$statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+}
+
+echo '<br />query version 3:<br />';
+$statement = $db->query('SELECT username, password FROM note_user');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
