71e438859aa3950f741f57cff119f4a2edea92a3
asquire <32476451+asquire@users.noreply.github.com>
2018-10-16 19:44:04 -0400

    Added 2 prepared statements

--- web/prepare05.php
+++ web/prepare05.php
@@ -39,4 +39,17 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 echo '<br />query version 3:<br />';
 $statement = $db->query('SELECT username, password FROM note_user');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo $results;
+
+echo '<br />query version 4:<br />';
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<br />query version 5:<br />';
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->execute(array(':name' => $name, ':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
