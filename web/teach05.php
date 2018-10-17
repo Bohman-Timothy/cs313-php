1975f3f172bf522ad1cdac3428edd82706828d75
asquire <32476451+asquire@users.noreply.github.com>
2018-10-16 23:43:53 -0400

    re-pasted the PHP

--- web/teach05.php
+++ web/teach05.php
@@ -1,4 +1,5 @@
 <?php
+
 try
 {
 	$dbUrl = getenv('DATABASE_URL');
@@ -23,31 +24,35 @@ catch (PDOException $ex)
 
 echo '<h1>Scripture Resources</h1>';
 
-foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row)
+$statement = $db->query('SELECT book, chapter, verse, content FROM scripture');
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	echo '<strong>' . $row['book'] . ' ';
-	echo $row['chapter'] . ':' . $row['verse'] . '</strong> - ';
+	echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
 	echo '&quot;' . $row['content'] . '&quot;';
 	echo '<br/>';
 }
+
 
 $search_book = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$search_book = clean_input($_POST["search_book"]);
 
-$statement = $db->prepare('SELECT book, chapter, verse, content FROM scripture WHERE book=:book');
-$statement->bindValue(':book', $search_book, PDO::PARAM_STR);
-$statement->execute();
+	$statement = $db->prepare('SELECT book, chapter, verse, content FROM scripture WHERE book=:book');
+	$statement->bindValue(':book', $search_book, PDO::PARAM_STR);
+	$statement->execute();
 
 
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
 		echo '<strong>' . $row['book'] . ' ';
-echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
-	echo '&quot;' . $row['content'] . '&quot;';
+		echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
+		echo '&quot;' . $row['content'] . '&quot;';
 		echo '<br/>';
-}
+	}
 }
 
 ?>
+
