dc82ca70c1b7fb38437b5427d2100a3bdcdc5036
asquire <32476451+asquire@users.noreply.github.com>
2018-10-17 21:50:24 -0400

    Moved try-catch to functions page

--- web/teach05.php
+++ web/teach05.php
@@ -2,28 +2,6 @@
 
 include 'teach05_functions.php';
 
-try
-{
-	$dbUrl = getenv('DATABASE_URL');
-	
-	$dbOpts = parse_url($dbUrl);
-	
-	$dbHost = $dbOpts["host"];
-	$dbPort = $dbOpts["port"];
-	$dbUser = $dbOpts["user"];
-	$dbPassword = $dbOpts["pass"];
-	$dbName = ltrim($dbOpts["path"],'/');
-
-	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-	
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-	echo 'Error!: ' . $ex->getMessage();
-	die();
-}
-
 echo '<h1>Scripture Resources</h1>';
 
 $statement_original = $db->query('SELECT book, chapter, verse, content FROM scripture');
