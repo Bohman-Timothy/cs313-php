6ef2377c023e574725836cf64182457ae4d782d0
asquire <32476451+asquire@users.noreply.github.com>
2018-10-17 20:20:21 -0400

    Made repeated code into a function

--- web/teach05.php
+++ web/teach05.php
@@ -26,29 +26,33 @@ echo '<h1>Scripture Resources</h1>';
 
 $statement = $db->query('SELECT book, chapter, verse, content FROM scripture');
 
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	echo '<strong>' . $row['book'] . ' ';
-	echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
-	echo '&quot;' . $row['content'] . '&quot;';
-	echo '<br/>';
-}
-
-function clean_input($data) {
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-   return $data;
-}
+showAllRows($statement);
 
 $search_book = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$search_book = clean_input($_POST["search_book"]);
+	$search_book = cleanInput($_POST["search_book"]);
 
 	$statement = $db->prepare('SELECT book, chapter, verse, content FROM scripture WHERE book=:book');
 	$statement->bindValue(':book', $search_book, PDO::PARAM_STR);
 	$statement->execute();
+}
+
+function showAllRows($statement) {
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<strong>' . $row['book'] . ' ';
+		echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
+		echo '&quot;' . $row['content'] . '&quot;';
+		echo '<br/>';
+	}
+}
+
+function cleanInput($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
 }
 
 ?>
@@ -64,13 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-		echo '<strong>' . $row['book'] . ' ';
-		echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
-		echo '&quot;' . $row['content'] . '&quot;';
-		echo '<br/>';
-	}
+	showAllRows($statement);
 }
 ?>
 </body>
