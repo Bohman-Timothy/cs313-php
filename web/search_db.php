1d354ac63bde1313a0a9eec75a3c69a60b409c2a
asquire <32476451+asquire@users.noreply.github.com>
2018-10-19 02:20:32 -0400

    Made table column headers left-aligned

--- web/search_db.php
+++ web/search_db.php
@@ -50,7 +50,7 @@ function showFullListOfFeatures($statement) {
 	
 	echo '<table class="featureResults"> <thead><caption>Features Matching Search: ';
 	echo $search . '</caption></thead>';
-	echo '<tr><th>ID</th><th>Feature Title</th><th>Feature Year</th><th>Format</th><th>Format Year</th>';
+	echo '<tr class="searchResultsHeaderRow"><th>ID</th><th>Feature Title</th><th>Feature Year</th><th>Format</th><th>Format Year</th>';
 	echo '<th>Feature Set Title</th><th>Location</th><th>Existing Loan</th></tr>';
 	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	{
@@ -59,9 +59,7 @@ function showFullListOfFeatures($statement) {
 		echo '<td class="featureYear">' . $row['feature_year'] . '</td>';
 		echo '<td class="format">' . $row['format'] . '</td>';
 		echo '<td class="formatYear">' . $row['format_year'] . '</td>';
-		if ($row['feature_set_title'] != '') {
-			echo '<td class="featureSetTitle">' . $row['feature_set_title'] . '</td>';
-		}
+		echo '<td class="featureSetTitle">' . $row['feature_set_title'] . '</td>';
 		echo '<td class="location">' . $row['location'] . '</td>';
 		echo '<td class="existingLoan">' . $row['existing_loan'] . '</td></tr>';
 	}
@@ -101,6 +99,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <ul>
 <li>https://stackoverflow.com/questions/2491068/does-height-and-width-not-apply-to-span/37876264</li>
 <li>https://stackoverflow.com/questions/5684144/how-to-completely-remove-borders-from-html-table</li>
+<li><a href="https://stackoverflow.com/questions/35787892/default-value-in-select-query-for-null-values-in-postgres">Default value if null and cast data to another type</a></li>
+<li>https://www.rapidtables.com/web/html/html-codes.html</li>
 </ul>
 </body>
 </html>
