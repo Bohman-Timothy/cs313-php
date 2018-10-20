b0324da77c89f115a7ac16182a121b960ed0927a
asquire <32476451+asquire@users.noreply.github.com>
2018-10-20 13:45:19 -0400

    Moved <br /> and search loans option

--- web/search_db.php
+++ web/search_db.php
@@ -230,15 +230,15 @@ function cleanInput($data) {
 			<label for="featureTitleOption_id">Feature Title</label><br />
 			<input type="radio" name="searchType" value="featureSetTitle" id="featureSetTitleOption_id">
 			<label for="featureSetTitleOption_id">Feature Set Title</label><br />
-			<input type="radio" name="searchType" value="featureYear" id="featureYearOption_id"><br />
-			<label for="featureYearOption_id">Feature Year</label>
+			<input type="radio" name="searchType" value="featureYear" id="featureYearOption_id">
+			<label for="featureYearOption_id">Feature Year</label><br />
 			<input type="radio" name="searchType" value="format" id="formatOption_id">
 			<label for="formatOption_id">Format</label><br />
 			<input type="radio" name="searchType" value="patron" id="patronOption_id">
 			<label for="patronOption_id">Patron</label><br />
-			<input type="checkbox" name="searchLoans" id="searchLoans_id">
-			<label for="searchLoans_id">Search Loans</label><br />
 		</div>
+		<input type="checkbox" name="searchLoans" id="searchLoans_id">
+		<label for="searchLoans_id">Search Loans</label><br />
 		<input type="submit" value="Search" class="submitButton">
 	</form>
 <?php
