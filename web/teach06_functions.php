613122a86e48b76ded8cec627ce0b31ac0da7cea
asquire <32476451+asquire@users.noreply.github.com>
2018-10-23 22:47:49 -0400

    Added teach06_functions.php

---
+++ web/teach06_functions.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Timothy
+ * Date: 10/23/2018
+ * Time: 10:39 PM
+ */
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+?>
+
+function showAllScriptures($db) {
+	$statement = $db->prepare('SELECT * FROM scripture');
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<strong>' . $row['book'] . ' ';
+		echo $row['chapter']. ':' . $row['verse'] . '</strong> - ';
+		echo '&quot;' . $row['content'] . '&quot;';
+		echo 'Topics: ';
+
+$statementScripTopic = $db->prepare('SELECT * FROM scriptures_topics LEFT JOIN topic on fk_scripture_id=:row' AND fk_topic_id = topic.id);
+$statementScripTopic.bindValue(:row, $row['id'], PDO::INT);
+$statementScripTopic.execute();
+
+		while ($rowTopic = $statementScripTopic->fetch(PDO::FETCH_ASSOC))
+{
+	//print topics
+	echo 'Topic: ' . $rowTopic['name'] . <br/>;
+}
+		echo '<br/>';
+	}
+}
+
+function showAllResultsScriptureReferences($statement) {
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<a href ="scripture_details.php?id=' . $row['id'] . '"><strong>' . $row['book'] . ' ';
+		echo $row['chapter']. ':' . $row['verse'] . '</strong></a>';
+		echo '<br/>';
+	}
+}
+
+function cleanInput($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+
+?>
