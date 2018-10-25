06709e38f8981435a5c7dce8812074b393db9f5e
asquire <32476451+asquire@users.noreply.github.com>
2018-10-25 01:16:30 -0400

    Tried moving most of the PHP code on the page to the beginning of the document again

--- web/scriptures_and_topics.php
+++ web/scriptures_and_topics.php
@@ -8,6 +8,48 @@
 
 include 'teach06_functions.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $book = cleanInput($_POST["book"]);
+    $chapter = cleanInput($_POST["chapter"]);
+    $verse = cleanInput($_POST["verse"]);
+    $content = cleanInput($_POST["content"]);
+    $topics = $_POST["topic"];
+    $newTopicCheckbox = $_POST["newTopicCheckbox"];
+    $newTopic = cleanInput($_POST["newTopic"]);
+
+    //insert topic
+    if (($newTopicCheckbox != '') && ($newTopic != '')) {
+        $statement_newTopic = $db->prepare('INSERT INTO topic (name) VALUES (:newTopic)');
+        $statement_newTopic->bindValue(':newTopic', $newTopic, PDO::PARAM_STR);
+        $statement_newTopic->execute();
+        echo 'Inserted new topic: ' . $newTopic . '<br/>';
+
+        /*$statement_newTopicId = $db->prepare('SELECT id FROM topic where name = :newTopic');
+        $statement_newTopicId->bindValue(':newTopic', $newTopic, PDO::PARAM_STR);
+        $statement_newTopicId->execute();
+
+        while ($row = $statement_newTopicId->fetch(PDO::FETCH_ASSOC))
+        {
+            $newTopicId =  $row['id'];
+        }*/
+        $newTopicId = $db->lastInsertId('topic_id_seq');
+        array_push($topics, $newTopicId);
+        echo 'New topic ID:' . $newTopicId . '<br/>';
+        print_r($topics);
+    }
+
+    //insert scripture
+    $statement = $db->prepare('INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
+    $statement->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
+
+    $scripture_id = $db->lastInsertId('scripture_id_seq');
+
+    foreach ($topics as $topic) {
+        //insert scripture_topic
+        $statement = $db->prepare('INSERT INTO scriptures_topics (fk_scripture_id, fk_topic_id) VALUES (:scripture, :topic)');
+        $statement->execute(array(':scripture' => $scripture_id, ':topic' => $topic));
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -64,50 +106,7 @@ include 'teach06_functions.php';
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $book = cleanInput($_POST["book"]);
-    $chapter = cleanInput($_POST["chapter"]);
-    $verse = cleanInput($_POST["verse"]);
-    $content = cleanInput($_POST["content"]);
-    $topics = $_POST["topic"];
-    $newTopicCheckbox = $_POST["newTopicCheckbox"];
-    $newTopic = cleanInput($_POST["newTopic"]);
-
-//insert topic
-    if (($newTopicCheckbox != '') && ($newTopic != '')) {
-        $statement_newTopic = $db->prepare('INSERT INTO topic (name) VALUES (:newTopic)');
-        $statement_newTopic->bindValue(':newTopic', $newTopic, PDO::PARAM_STR);
-        $statement_newTopic->execute();
-        echo 'Inserted new topic: ' . $newTopic. '<br/>';
-
-        /*$statement_newTopicId = $db->prepare('SELECT id FROM topic where name = :newTopic');
-        $statement_newTopicId->bindValue(':newTopic', $newTopic, PDO::PARAM_STR);
-        $statement_newTopicId->execute();
-
-        while ($row = $statement_newTopicId->fetch(PDO::FETCH_ASSOC))
-        {
-            $newTopicId =  $row['id'];
-        }*/
-        $newTopicId = $db->lastInsertId('topic_id_seq');
-        array_push($topics, $newTopicId);
-        echo 'New topic ID:' . $newTopicId . '<br/>';
-        print_r($topics);
-    }
-
-//insert scripture
-    $statement = $db->prepare('INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
-    $statement->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
-
-    $scripture_id = $db->lastInsertId('scripture_id_seq');
-
-    foreach ($topics as $topic)
-    {
-        //insert scripture_topic
-        $statement = $db->prepare('INSERT INTO scriptures_topics (fk_scripture_id, fk_topic_id) VALUES (:scripture, :topic)');
-        $statement->execute(array(':scripture' => $scripture_id, ':topic' => $topic));
-    }
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //display all scriptures
     showAllScriptures($db);
 }
