ebedb38537fe2b71949d2b46db7f8630fee1e237
asquire <32476451+asquire@users.noreply.github.com>
2018-10-26 00:28:54 -0400

    added enterFeatureId variable declaration

--- web/update_db.php
+++ web/update_db.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $updateFeature = $_POST["updateFeature"];
+    $enterFeatureId = $_POST["enterFeatureId"];
 
     //insert topic
     if (($updateFeature != '') && ($enterFeatureId != '')) {
