7d9936e83f0203cd701554d50c529b9453a0068a
asquire <32476451+asquire@users.noreply.github.com>
2018-10-26 21:04:52 -0400

    Made text and button appear according to last action

--- web/update_db.php
+++ web/update_db.php
@@ -1,38 +1,46 @@
 <?php
 include 'project1_functions.php';
 
+$updateFeature = $enterFeatureId = $action = '';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $updateFeature = $_POST["updateFeature"];
     $enterFeatureId = $_POST["enterFeatureId"];
+    $action = $_POST["action"];
 
-    //insert topic
-    if (($updateFeature != '') && ($enterFeatureId != '')) {
+    //select ID and display the feature
+    if (($action == 'Select ID') && ($updateFeature != '') && ($enterFeatureId != '')) {
         echo 'Feature ID to update: ' . $updateFeature . '<br/>';
 
         $db_query_feature_id = 'SELECT id, feature_title, feature_year, format, format_year, feature_set_title, location, existing_loan FROM feature_view WHERE id = ' . $updateFeature . ';';
         $db_statement_feature_id = $db->prepare($db_query_feature_id);
         $db_statement_feature_id->execute();
 
+        //show selected feature
         echo '<table class="featureResults">';
         echo '<thead><caption class="exactResultsTableCaption">Result Matching Search</caption></thead>';
         showFullListOfFeatures($db_statement_feature_id);
-        /*while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+
+        //get values to populate input
+        $counter = 0;
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
-            echo '<tr><td class="id">' . $row['id'] . '</td>';
-            echo '<td class="featureTitle">' . $row['feature_title'] . '</td>';
-            echo '<td class="featureYear">' . $row['feature_year'] . '</td>';
-            echo '<td class="format">' . $row['format'] . '</td>';
-            echo '<td class="formatYear">' . $row['format_year'] . '</td>';
-            echo '<td class="featureSetTitle">' . $row['feature_set_title'] . '</td>';
-            echo '<td class="location">' . $row['location'] . '</td>';
-            echo '<td class="existingLoan">' . $row['existing_loan'] . '</td></tr>';
+            $id = $row['id'];
+            $featureTitle = $row['feature_title'];
+            $featureYear = $row['feature_year'];
+            $format = $row['format'];
+            $formatYear = $row['format_year'];
+            $featureSetTitle = $row['feature_set_title'];
+            $location = $row['location'];
+            $existingLoan = $row['existing_loan'];
+            $counter++;
         }
         if ($counter == 0) {
             echo '</table> <p class=\'noResults\'>No results</p>';
         }
         else {
             echo '</table>';
-        */
+
         //insert scripture
         /*$statement = $db->prepare('INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
         $statement->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
@@ -66,6 +74,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			<label for="enterFeatureId_id">Enter Feature ID:</label>
             <input type="number" name="enterFeatureId" id="enterFeatureId_id" title="Enter the ID found by using the database's search feature" value="">
             <input type="submit" name="action" value="Select ID" class="submitButton" id="selectIdButton_id" formnovalidate onclick="if(document.getElementById('updateFeatureCheckbox_id').checked){document.getElementById('updateFeatureCheckbox_id').value = document.getElementById('enterFeatureId_id').value;}"><br />
+            <?php
+            if ($updateFeature != '') {
+                echo '<p>Feature ' . $updateFeature . ' has been selected.</p>';
+            }
+            ?>
         </div>
 		<label for="addFeatureTitle_id">Feature Title:</label>
 		<input type="text" name="addFeatureTitle" id="addFeatureTitle_id" title="Enter exact title of the feature" required value=""><br />
@@ -99,7 +112,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			<input type="radio" name="addLocation" value="livingRoom" id="livingRoomOption_id">
 			<label for="livingRoomOption_id">Living Room</label><br />
 		</div>
-		<input type="submit" name="action" value="Add or Update" class="submitButton" id="addOrUpdateButton_id" onclick="if(document.getElementById('updateFeatureCheckbox_id').checked){document.getElementById('updateFeatureCheckbox_id').value = document.getElementById('enterFeatureId_id').value;}">
+        <?php
+        if ($updateFeature != '') {
+            echo '<input type="submit" name="action" value="Update Feature" class="submitButton" id="updateFeatureButton_id">';
+        }
+        else {
+            echo '<input type="submit" name="action" value="Add Feature" class="submitButton" id="addFeatureButton_id">';
+        }
+        ?>
+		<!-- <input type="submit" name="action" value="Add or Update" class="submitButton" id="addOrUpdateButton_id" onclick="if(document.getElementById('updateFeatureCheckbox_id').checked){document.getElementById('updateFeatureCheckbox_id').value = document.getElementById('enterFeatureId_id').value;}"> -->
 	</form>
 </body>
 </html>
