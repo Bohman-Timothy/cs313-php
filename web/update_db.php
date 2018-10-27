fe718d20d0a153db75c362133d3ac21af9f66c22
asquire <32476451+asquire@users.noreply.github.com>
2018-10-27 17:08:52 -0400

    Tweaking select query

--- web/update_db.php
+++ web/update_db.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     //select ID and display the feature
     else if (($action == 'Select ID') && ($updateFeature != '') && ($featureId != '')) {
-        $db_query_feature_id = 'SELECT fv.id, fv.feature_title, fv.feature_year, f.format, fv.format_year, fv.feature_set_title, f.location, fv.existing_loan FROM feature_view fv LEFT JOIN feature f on fv.id = f.id WHERE fv.id = ' . $updateFeature . ';';
+        $db_query_feature_id = 'SELECT fv.id, f.id, fv.feature_title, fv.feature_year, f.format, fv.format_year, fv.feature_set_title, f.location, fv.existing_loan FROM feature_view fv LEFT JOIN feature f on fv.id = f.id WHERE fv.id = ' . $updateFeature . ';';
         $db_statement_feature_id = $db->prepare($db_query_feature_id);
         $db_statement_feature_id->execute();
 
@@ -77,7 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $counter = 0;
         while ($row = $db_statement_feature_id->fetch(PDO::FETCH_ASSOC))
         {
-            $featureId = $row['id'];
+            $featureId = $row['fv.id'];
             $featureTitle = $row['feature_title'];
             $featureYear = $row['feature_year'];
             $format = $row['format'];
