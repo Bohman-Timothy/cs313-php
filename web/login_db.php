1e0c4447e00b4fb447edca752dc6d53cff907b45
asquire <32476451+asquire@users.noreply.github.com>
2018-10-27 20:00:43 -0400

    Added 'Logged in as' text. Added ability to log out.

--- web/login_db.php
+++ web/login_db.php
@@ -6,29 +6,34 @@ $successMessage = $errorMessage = '';
 $username = $password = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $submitAction = $_POST["submit"];
     $username = cleanInput($_POST["username"]);
     $password = cleanInput($_POST["password"]);
 
-    if (($username != '') && ($password != '')) {
-        $db_query_username = 'SELECT id, username, full_name FROM patron WHERE username =:username and password =:password;';
-        $db_statement_username = $db->prepare($db_query_username);
-        $db_statement_username->execute(array(':username' => $username, ':password' => $password));
-        /*$result = $db_statement_username->get_result();*/
+    if ($submitAction == 'Log In') {
+        if (($username != '') && ($password != '')) {
+            $db_query_username = 'SELECT id, username, full_name FROM patron WHERE username =:username and password =:password;';
+            $db_statement_username = $db->prepare($db_query_username);
+            $db_statement_username->execute(array(':username' => $username, ':password' => $password));
+            /*$result = $db_statement_username->get_result();*/
 
-        $match = false;
-        while ($row = $db_statement_username->fetch(PDO::FETCH_ASSOC))
-        {
+            $match = false;
+            while ($row = $db_statement_username->fetch(PDO::FETCH_ASSOC)) {
                 $match = true;
-        }
+            }
 
-        if (($match == true) && (($username == 'tester') || ($username == 'asquire'))) {
-            $_SESSION["user"] = $username;
-            $_SESSION["loggedIn"] = true;
-            $successMessage = '<p class="successMessage">Successfully logged in as ' . $_SESSION["user"] . '</p>';
+            if (($match == true) && (($username == 'tester') || ($username == 'asquire'))) {
+                $_SESSION["user"] = $username;
+                $_SESSION["loggedIn"] = true;
+                $successMessage = '<p class="successMessage">Successfully logged in as ' . $_SESSION["user"] . '</p>';
+            } else {
+                $errorMessage = '<p class="errorMessage">Incorrect username or password</p>';
+            }
         }
-        else {
-            $errorMessage = '<p class="errorMessage">Incorrect username or password</p>';
-        }
+    }
+    else if ($submitAction == 'Log Out') {
+        session_unset();
+        session_destroy();
     }
 }
 ?>
@@ -51,21 +56,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <li class="active">
         <?php
             if ($_SESSION["loggedIn"] == true) {
-                echo '<a href="login_db.php">Sign Out</a>';
+                echo '<a href="login_db.php">Sign Out</a><br/><span class="loggedInAsUser">Logged in as ' . $_SESSION["user"] . '</span>';
             }
             else {
                 echo '<a href="login_db.php">Sign In</a>';
             }
         ?></li>
 </ul>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" name="checkout">
-    <h2>Enter username and password</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" name="login">
+    <h2>Enter username and password to log in</h2>
     <label for="username_id">Enter Username:</label>
     <input type="text" name="username" id="username_id" required value=""><br/>
     <label for="password_id">Enter Password:</label>
     <input type="password" name="password" id="password_id" required><br/>
-    <input type="submit" value="Submit" class="submitButton">
+    <input type="submit" name="submit" value="Log In" class="submitButton">
 </form>
+<?php
+if ($_SESSION["loggedIn"] == true) { ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="logout">
+        <h2>Use the button below to log out</h2>
+        <input type="submit" name="submit" value="Log Out" class="submitButton">
+    </form>
+<?php
+}
+?>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($successMessage != '') {
