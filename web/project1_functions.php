3d75bfad40f353aa95a5701bcee55494bbe9a7dc
asquire <32476451+asquire@users.noreply.github.com>
2018-10-31 04:27:29 -0400

    Put NULL in quotes again

--- web/project1_functions.php
+++ web/project1_functions.php
@@ -210,7 +210,7 @@ function returnFeatureLoan($featureId, $db) {
     $db_query_update_current_loan = 'UPDATE current_loan SET fk_loan = :loanId, updated_at = :updatedAt WHERE fk_feature = :featureId;';
     echo '<p>' . $db_query_update_current_loan . '</p>';
     $db_statement_update_current_loan = $db->prepare($db_query_update_current_loan);
-    $db_statement_update_current_loan->execute(array(':featureId' => $featureId, ':loanId' => NULL, ':updatedAt' => 'now()'));
+    $db_statement_update_current_loan->execute(array(':featureId' => $featureId, ':loanId' => 'NULL', ':updatedAt' => 'now()'));
     echo '<p>Successfully updated current loan status to NULL for feature #' . $featureId . '</p>';
 
     //update feature loan status to "No"
