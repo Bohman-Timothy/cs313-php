92500087700f91ba4e64e580082641afdb94f398
asquire <32476451+asquire@users.noreply.github.com>
2018-10-30 23:56:54 -0400

    Added JavaScript form checking to replace PHP version

--- web/signup.php
+++ web/signup.php
@@ -10,20 +10,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $confirmPassword = cleanInput($_POST["confirmPassword"]);
 
     //Compare passwords
-    if ($password === $confirmPassword) {
+    /*if ($password === $confirmPassword) {
         if ((strlen($password) > 6) && (preg_match('/[0-9]/', $password)))
-        {
+        {*/
             // Hash the password
             $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-// Insert password
+            // Insert password
             $statement = $db->prepare('INSERT INTO user_info (username, password) VALUES (:user, :pass)');
             $statement->execute(array(':user' => $username, ':pass' => $passwordHash));
 
-// Go to sign in screen
+            // Go to sign in screen
             header('Location: signin.php');
             die();
-        }
+/*        }
         else
         {
             $insufficientPassword = true;
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 //Passwords don't match!
         $passwordsDontMatch = true;
         $passwordError = true;
-    }
+    }*/
 }
 ?>
 
@@ -51,6 +51,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         color: red;
     }
 </style>
+<script>
+    function validateNewUser() {
+        var password = document.getElementById('password');
+        var confirmPassword = document.getElementById('confirmPassword');
+
+        var errorMessages = document.getElementsByClassName('error');
+
+        if (password !== confirmPassword)
+        {
+            for (var i = 0; i < errorMessages.length; ++i)
+            {
+                errorMessages[i].style.display = 'block';
+            }
+            document.getElementById('errorMessage').content = "Passwords don't match.";
+        }
+
+        if ((password.length > 6) && (password.search(/[0-9]/))) {
+
+        }
+        else {
+            for (var i = 0; i < errorMessages.length; ++i)
+            {
+                errorMessages[i].style.display = 'block';
+            }
+            document.getElementById('errorMessage').content = "Password must be at least 7 characters long and contain at least one number.";
+        }
+    }
+</script>
 <body>
 <h1>Sign-Up</h1>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="signUpForm">
@@ -63,13 +91,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     <input type="password" name="confirmPassword" id="confirmPassword"><?php if($passwordError == true){echo '<span class="error">*</span>';} ?><br/>
     <input type="submit" value="Submit"><br/>
 </form>
+<!-- <form method="post" action="<?php /*echo htmlspecialchars($_SERVER["PHP_SELF"]);*/ ?>" id="signUpForm">
+    <h2>Enter login information</h2>
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username"><br/>
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password"><?php /*if($passwordError == true){echo '<span class="error">*</span>';}*/ ?><br/>
+    <label for="confirmPassword">Confirm Password:</label>
+    <input type="password" name="confirmPassword" id="confirmPassword"><?php /*if($passwordError == true){echo '<span class="error">*</span>';}*/ ?><br/>
+    <input type="submit" value="Submit"><br/>
+</form> -->
 <?php
-if($passwordsDontMatch == true) {
+/*if($passwordsDontMatch == true) {
     echo '<span class="error">Passwords don\'t match.</span>';
 }
 if($insufficientPassword == true) {
     echo '<span class="error">Password must be at least 7 characters long and contain at least one number.</span>';
-}
+}*/
 ?>
 </body>
 </html>
