fcb8e7ab59a471deadd3d53c618022ec9627b151
asquire <32476451+asquire@users.noreply.github.com>
2018-11-01 22:32:15 -0400

    Testing 'AND ! ~ *' addition to regex search 3 - put column name outside of quotes

--- web/search_db.php
+++ web/search_db.php
@@ -86,7 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 						default: //featureTitle, featureSetTitle, format
 							$db_query_exact = 'SELECT id, feature_title, feature_year, format, format_year, feature_set_title, location, existing_loan FROM feature_view WHERE ' . $searchTargetColumn . ' = \'' . $searchInput . '\' ' . $orderBy . ';';
 
-							$db_query_regexp = 'SELECT id, feature_title, feature_year, format, format_year, feature_set_title, location, existing_loan FROM feature_view WHERE ' . $searchTargetColumn . ' ~* \'.*' . preg_quote($searchInput) . '.*\' AND . $searchTargetColumn !~* \'' . $searchInput . '\' ' . $orderBy . ';';
+							$db_query_regexp = 'SELECT id, feature_title, feature_year, format, format_year, feature_set_title, location, existing_loan FROM feature_view WHERE ' . $searchTargetColumn . ' ~* \'.*' . preg_quote($searchInput) . '.*\' AND ' . $searchTargetColumn . ' !~* \'' . $searchInput . '\' ' . $orderBy . ';';
 					}
 			}
 
