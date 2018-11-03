dcc0934528dbfe083428810771c8db961fb1327b
asquire <32476451+asquire@users.noreply.github.com>
2018-11-03 00:59:45 -0400

    Added echo into the input field

--- web/checkout_db.php
+++ web/checkout_db.php
@@ -79,8 +79,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo '<p>Successfully retrieved borrower ID: ' . $borrowerId . '</p>';
 
             //Restrict returns to the borrower or an admin
-            //if (($borrowerId == $_SESSION["userId"]) || ($_SESSION["user"] == 'asquire')) {
-            if (true) {
+            if (($borrowerId == $_SESSION["userId"]) || ($_SESSION["user"] == 'asquire')) {
+            //if (true) {
                 returnFeatureLoan($_SESSION["featureId"], $db);
                 $successMessage = '<p class="successMessage">The feature has been marked as returned.</p>';
             }
@@ -129,7 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="searchIdForCheckout_id">
 		<h2>Enter data to select a feature to get on loan</h2>
 		<label for="enterFeatureId_id">Enter Feature ID:</label>
-		<input type="number" min="1" name="featureId" id="enterFeatureId_id" title="Enter the ID found by using the database's search feature" required value=""><br />
+		<input type="number" min="1" name="featureId" id="enterFeatureId_id" title="Enter the ID found by using the database's search feature" required value="<?php echo $featureId; ?>"><br />
 		<input type="submit" name="submit" value="Search" class="submitButton" id="searchIdButton_id">
 	</form>
     <?php
@@ -149,7 +149,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <h2>Add the feature listed above to your checkout list?</h2>
                         <!-- <input type="checkbox" name="addToCheckout" id="addToCheckout_id">
                         <label for="addToCheckout_id">Yes, add to checkout list</label><br/> -->
-                        <input type="number" min="1" name="selectedFeatureInputHidden" value="<?php echo $featureId ?>"
+                        <input type="number" min="1" name="selectedFeatureInputHidden" value="<?php echo $featureId; ?>"
                                id="selectedFeatureInputHidden_id">
                         <input type="submit" name="submit" value="Confirm Checkout" class="submitButton" id="confirmAddToCheckoutButton_id">
                         <input type="submit" name="submit" value="Clear Selection" class="submitButton"
@@ -162,7 +162,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     ?>
                     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" id="checkout_id">
                         <h2>Return This Feature?</h2>
-                        <input type="number" min="1" name="selectedFeatureInputHidden" value="<?php echo $featureId ?>"
+                        <input type="number" min="1" name="selectedFeatureInputHidden" value="<?php echo $featureId; ?>"
                                id="selectedFeatureInputHidden_id">
                         <input type="submit" name="submit" value="Confirm Return" class="submitButton" id="confirmReturnFeatureButton_id">
                         <input type="submit" name="submit" value="Clear Selection" class="submitButton"
